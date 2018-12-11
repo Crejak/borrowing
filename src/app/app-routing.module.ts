0423fefa11d662ba0d0f9ba459f3221bc2305547
Arthur BELLEMIN <arthurbellemin11@gmail.com>
2018-12-11 11:40:51 +0100

    Ajouté routing et components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/login/login.component';
+import { NotfoundComponent } from '../app/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: '*', component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
