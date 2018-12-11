78bc5cb980002ffa0084c9caebbdfc23b521e971
Abdelaziz97 <eabdelaziz97@gmail.com>
2018-12-11 10:18:36 +0100

    essai correction angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Trocampus'`, () => {
+  it(`should have as title 'trocampus'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Trocampus');
+    expect(app.title).toEqual('trocampus');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Trocampus!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to trocampus!');
   });
 });
