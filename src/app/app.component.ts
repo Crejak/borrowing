b745b045414a2b28fe25c8791c13778ae6eba1a2
Mazine Assimi LE BG <mazine.assimi&insa-lyon.fr>
2018-12-13 17:22:20 +0100

    added developper mode control (F6)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { HeaderComponent } from '../app/header/header.component';
+
+import { UtilService } from './util.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,15 @@ import { HeaderComponent } from '../app/header/header.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
+  constructor(private utilService: UtilService) {
+
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (event.code === "F6") {
+      this.utilService.developperMode = ! this.utilService.developperMode;
+    }
+  }
 }
--- src/app/util.service.ts
+++ src/app/util.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class UtilService {
 
+  public developperMode: boolean = true;
+
   constructor() { }
 
   getApplicationName(): string {
