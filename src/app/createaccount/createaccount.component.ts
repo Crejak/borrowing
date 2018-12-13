0a8ebf10d50c5eef53b59020ebca8c9e101575df
Abdelaziz97 <eabdelaziz97@gmail.com>
2018-12-13 15:27:36 +0100

    create account html

--- src/app/createaccount/createaccount.component.ts
+++ src/app/createaccount/createaccount.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilService } from '../util.service';
 
 @Component({
   selector: 'app-createaccount',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateaccountComponent implements OnInit {
 
-  constructor() { }
+  title : string;
+
+  constructor(private utilService: UtilService) { }
 
   ngOnInit() {
+    this.title = this.utilService.getApplicationName();
   }
 
 }
